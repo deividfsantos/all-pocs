81622d40b95278ce7ec0390a9e29db26c5e1a3f5
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-01-28 15:29:33 -0300

    finish hello endpoint

--- ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
+++ ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
@@ -1,12 +1,12 @@
 package com.dsantos;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DontPadController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String getIndex() {
         return "Hello World";
     }
