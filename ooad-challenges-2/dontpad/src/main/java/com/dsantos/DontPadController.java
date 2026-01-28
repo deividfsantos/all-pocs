4303742afc80f59dccfd4fe7854238e42de4c5f6
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-01-28 15:26:31 -0300

    add spring web and endpoint

--- ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
+++ ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
@@ -1,4 +1,13 @@
 package com.dsantos;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DontPadController {
+
+    @GetMapping
+    public String getIndex() {
+        return "Hello World";
+    }
 }
