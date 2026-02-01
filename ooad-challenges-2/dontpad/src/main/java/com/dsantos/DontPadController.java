c3dd783ab128182d210574530db2ba5125c92f21
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-02-01 14:13:26 -0300

    change endpoint to default one

--- ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
+++ ooad-challenges-2/dontpad/src/main/java/com/dsantos/DontPadController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DontPadController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String getIndex() {
         return "Hello World";
     }
