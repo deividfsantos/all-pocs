c39a2401f54d63e1577fb0fc6052cf9e028c9b2c
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-02-08 00:41:00 -0300

    static html working for all routes

--- ooad-challenges-2/dontpad/src/main/java/com/dsantos/HomeController.java
+++ ooad-challenges-2/dontpad/src/main/java/com/dsantos/HomeController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/{path:[^\\.]*}")
     public String anyPath() {
-        return "index";
+        return "forward:/index.html";
     }
 }
